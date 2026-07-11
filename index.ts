71b6c5683b20e5e7b5f618c27f01014e949b4371
musazahidtamim3-ai <musazahidtamim3@gmail.com>
2026-07-11 20:59:53 +0300

    userscollection created

---
+++ index.ts
@@ -0,0 +1,61 @@
+import express, { type Request, type Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(process.env.MONGODB_URI as string, {
+     serverApi: {
+          version: ServerApiVersion.v1,
+          strict: true,
+          deprecationErrors: true,
+     },
+});
+
+async function startServer() {
+     await client.connect();
+     console.log("MongoDB Connected");
+
+     const db = client.db(process.env.DB_NAME);
+     const usersCollection = db.collection("users");
+
+     app.get("/", (req: Request, res: Response) => {
+          res.send("Reports Zone Server is Running...");
+     });
+
+     // Save User
+     app.post("/api/users", async (req: Request, res: Response) => {
+          try {
+               const user = req.body;
+               const result = await usersCollection.insertOne(user);
+               res.status(201).send(result);
+          } catch (error) {
+               console.error(error);
+               res.status(500).send({ message: "Something went wrong" });
+          }
+     });
+
+     // Get Users
+     app.get("/api/users", async (req: Request, res: Response) => {
+          try {
+               const users = await usersCollection.find().toArray();
+               res.send(users);
+          } catch (error) {
+               console.error(error);
+               res.status(500).send({ message: "Something went wrong" });
+          }
+     });
+
+     app.listen(port, () => {
+          console.log(` Server Running on http://localhost:${port}`);
+     });
+}
+
+startServer();
